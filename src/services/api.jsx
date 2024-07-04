f976ea19da0441afc29b391aaf6580bd4a3768af
IsraelPontes <israelcpontes@hotmail.com>
2024-07-04 16:16:25 -0300

    Updated API baseURL to HTTPS to fix Mixed Content error

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://api.themoviedb.org/3/',
+    baseURL: 'https://api.themoviedb.org/3/',
     params:{
         api_key: '660a9575a5b0f97960a740ccbb4b4b3e',
         language: 'pt-br',
